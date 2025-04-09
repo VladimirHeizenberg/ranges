60ca9089114df9b62223e4a031e558c064e92ae4
Vladimir <volodiakalganov@gmail.com>
2025-04-09 21:52:44 +0300

    out

---
+++ lib/Out.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Range.h"
+#include "Write.h"
+
+class Out : public Pipe {
+public:
+    Out(std::ostream& out)
+    : out_(out){}
+
+    template<class Range>
+    Range& operator()(const Range& range) const {
+        for (auto& item : range) {
+            out_ << item << '\n';
+        }
+        return range;
+    }
+  
+private:
+    std::ostream& out_;
+};
